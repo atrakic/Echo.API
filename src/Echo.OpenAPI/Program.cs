f9707b25d625284da91df3bcb15eecb3da1c4abf
Admir Trakic <atrakic@users.noreply.github.com>
2024-06-09 01:27:50 +0200

    chore: add routes

--- src/Echo.OpenAPI/Program.cs
+++ src/Echo.OpenAPI/Program.cs
@@ -24,27 +24,13 @@ builder.Services.AddAuthentication("Bearer").AddJwtBearer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<SwaggerGeneratorOptions>(o => { o.InferSecuritySchemes = true; });
 builder.Services.AddOpenApiDocument(config =>
 {
     config.DocumentName = "EchoAPI";
     config.Title = "EchoAPI v1";
     config.Version = "v1";
     config.Description = "A simple API to echo messages";
-
-    /**
-    config.AddSecurity("Bearer", new
-    {
-        Type = OpenApiSecuritySchemeType.Http,
-        Scheme = "bearer",
-        Bearer = new OpenApiSecurityScheme
-        {
-            Name = "Authorization",
-            Type = OpenApiSecuritySchemeType.ApiKey,
-            In = OpenApiSecurityApiKeyLocation.Header,
-            Description = "JWT Authorization header using the Bearer scheme."
-        }
-    });
-    */
 });
 
 var app = builder.Build();
@@ -68,5 +54,86 @@ app.MapGet("/api/messages", (IMessageRepository msgRepository) =>
 .WithName("GetMessageItems")
 .WithOpenApi();
 
+app.MapGet("/api/messages/{id:int}", Results<Ok<MessageItem>, NotFound> ([FromRoute] int id, IMessageRepository msgRepository) =>
+{
+    var item = msgRepository.GetItem(id);
+    return item != null ? TypedResults.Ok(item) : TypedResults.NotFound();
+})
+.WithName("GetMessageItem")
+.WithOpenApi();
+
+//POST a new message item
+app.MapPost("/api/messages",
+    [Authorize] (MessageItem item, IMessageRepository msgRepository) =>
+{
+    msgRepository.AddItem(item);
+    msgRepository.SaveChanges();
+    return Results.Created($"/api/messages/{item.Id}", item);
+})
+.WithName("PostMessageItem")
+.WithOpenApi(operation =>
+{
+    operation.Security = new List<OpenApiSecurityRequirement>
+    {
+       JWTSecurity.GetDefaultOpenApiSecurityRequirement()
+    };
+    return operation;
+}
+);
+
+// PUT an existing message item
+app.MapPut("/api/messages/{id:int}",
+    [Authorize] Results<NoContent, NotFound> (
+        [FromRoute] int id, MessageItem msgItem, IMessageRepository msgRepository) =>
+{
+    var existingItem = msgRepository.GetItem(id);
+
+    if (existingItem is null)
+    {
+        return TypedResults.NotFound();
+    }
+
+    existingItem.Name = msgItem.Name;
+    msgRepository.UpdateItem(existingItem);
+
+    msgRepository.SaveChanges();
+    return TypedResults.NoContent();
+
+})
+.WithName("UpdateMessageItem")
+.WithOpenApi(operation =>
+{
+    operation.Security = new List<OpenApiSecurityRequirement>
+    {
+        JWTSecurity.GetDefaultOpenApiSecurityRequirement()
+    };
+    return operation;
+});
+
+// DELETE an existing message item
+app.MapDelete("/api/messages/{id:int}",
+    [Authorize] Results<NoContent, NotFound> (
+        [FromRoute] int id, IMessageRepository msgRepository) =>
+{
+    var existingItem = msgRepository.GetItem(id);
+
+    if (existingItem is null)
+    {
+        return TypedResults.NotFound();
+    }
+
+    msgRepository.RemoveItem(existingItem);
+    msgRepository.SaveChanges();
+    return TypedResults.NoContent();
+})
+.WithName("DeleteMessageItem")
+.WithOpenApi(operation =>
+{
+    operation.Security = new List<OpenApiSecurityRequirement>
+    {
+        JWTSecurity.GetDefaultOpenApiSecurityRequirement()
+    };
+    return operation;
+});
 
 app.Run();
