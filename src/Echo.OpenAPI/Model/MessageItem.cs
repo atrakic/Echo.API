63611752a3832a31b6432329cf5641f0963a7952
Admir Trakic <atrakic@users.noreply.github.com>
2024-06-10 08:12:01 +0200

    Add date property

--- src/Echo.OpenAPI/Model/MessageItem.cs
+++ src/Echo.OpenAPI/Model/MessageItem.cs
@@ -3,6 +3,7 @@ namespace Echo.Model
     public class MessageItem
     {
         public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
         public string? Name { get; set; }
     }
 }
